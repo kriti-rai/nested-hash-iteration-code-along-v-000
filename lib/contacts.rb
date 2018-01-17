78e1519d3d0e8d5cb27b7b17d6581b55dee55cf4
Kriti Rai <kriti.rai@gmail.com>
2018-01-17 03:03:15 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|.delete_if
+    contact_details_hash.delete_if do |attribute, data|
       attribute == :favorite_ice_cream_flavors
     end
   end
